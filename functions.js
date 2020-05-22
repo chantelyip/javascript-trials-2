5db7b3c8a6fd89cfcf744f58d8a0d182d7e30771
Chantel Yip <chantelyip422@gmail.com>
2020-05-21 20:50:49 -0700

    completed calculateTotal function

--- functions.js
+++ functions.js
@@ -10,12 +10,35 @@ console.log(isHometown('San Francisco'))
 
 // 2. getFullName
 
-    function getFullName(firstName, lastName) {
-        return `${firstName} ${lastName}`;
-    }
-console.log(getFullName('Chantel', 'Yip'))
-    
+//     function getFullName(firstName, lastName) {
+//         return `${firstName} ${lastName}`;
+//     }
+// console.log(getFullName('Chantel', 'Yip'))
+
+// alternative way with arrow
+
+    const getFullName2 = (firstName, lastName) => `${firstName} ${lastName}`;
+console.log(getFullName2('Chantel', 'Yip'))
 
 // 3. calculateTotal
 
-// Define your function here
+    function calculateTotal(basePrice, state, tax=0.05) {
+
+        let fee = 0;
+        const subtotal = basePrice + (1 + tax);
+
+        if  (state === 'CA') {
+            fee = .03 * subtotal;
+        }   else if (state === 'PA') {
+            fee = 2;
+        }   else if  (state === 'MA') {
+            if (basePrice <= 100)   {
+                fee = 1;
+            } else {
+            fee = 3;
+            } 
+        }
+    return subtotal + fee 
+}
+console.log(calculateTotal(200, 'MA'))  
+
